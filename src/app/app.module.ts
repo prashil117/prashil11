949e59298997dcd82b980db7fa0c5ed5c6dc3771
Zeel Modi <zeel91297@gmail.com>
2018-11-07 23:08:05 +0530

    scroll done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {ScrollToModule} from 'ng2-scroll-to';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +12,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ScrollToModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
